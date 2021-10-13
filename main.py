a4cd65dba49f4fa3db2945e4a97e76b200f1b5f2
Muresan Flaviu <flaviucristi@yahoo.com>
2021-10-13 18:59:21 +0300

    Al treilea  commit

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ def numarprim(x):
     :param x: numer intreg
     :return: daca numarul este prim sau nu
     """
-    if x<2:
+    if x < 2:
         return False
     else:
-        for i in range(2,x//2+1):
-            if x%i==0:
+        for i in range(2, x // 2 + 1):
+            if x % i == 0:
                 return False
 
     return True
+
 
 def sumnr(list):
     """
@@ -19,10 +20,11 @@ def sumnr(list):
     :param list: numere intregi
     :return: suma numerelor
     """
-    s=0
+    s = 0
     for i in list:
-        s=s+int(i)
+        s = s + int(i)
     return s
+
 
 def get_longest_sum_is_prime(list):
     """
@@ -30,14 +32,13 @@ def get_longest_sum_is_prime(list):
     :param list: numere intregi
     :return:secventa maxima in care suma numerelor este numar prim
     """
-    subsec=[]
+    subsec = []
     for i in range(len(list)):
-        for j in range(i,len(list)):
-            if numarprim(sumnr(list[i:j+1]))==True and len(list[i:j+1])>len(subsec):
-                subsec=list[i:j+1]
+        for j in range(i, len(list)):
+            if numarprim(sumnr(list[i:j + 1])) == True and len(list[i:j + 1]) > len(subsec):
+                subsec = list[i:j + 1]
 
     return subsec
-
 
 
 def nrcifre(n):
@@ -46,13 +47,14 @@ def nrcifre(n):
     :param n: numar intreg
     :return: numarul de cifre
     """
-    c=int(n)
+    c = int(n)
 
-    t=0
-    while c>0:
-        t=t+1
-        c=c//10
+    t = 0
+    while c > 0:
+        t = t + 1
+        c = c // 10
     return t
+
 
 def numarcifredesc(list):
     """
@@ -60,10 +62,11 @@ def numarcifredesc(list):
     :param list: numere intregi
     :return: daca numarul de cifre ale numerelor se afla in ordine descrescatoare
     """
-    for i in range(len(list)-1):
-        if nrcifre(list[i])< nrcifre(list[i+1]):
+    for i in range(len(list) - 1):
+        if nrcifre(list[i]) < nrcifre(list[i + 1]):
             return False
     return True
+
 
 def get_longest_digit_count_desc(list):
     """
@@ -71,25 +74,48 @@ def get_longest_digit_count_desc(list):
     :param list: numere intregi
     :return:secventa maxima in care nuarul de cifre este in ordine descrescatoare
     """
-    secmax=[]
+    secmax = []
     for i in range(len(list)):
-        for j in range(i,len(list)):
-            if numarcifredesc(list[i:j+1])==True and len(list[i:j+1])>len(secmax):
-                secmax=list[i:j+1]
+        for j in range(i, len(list)):
+            if numarcifredesc(list[i:j + 1]) == True and len(list[i:j + 1]) > len(secmax):
+                secmax = list[i:j + 1]
 
     return secmax
 
 
+def numerepare(list):
+    for i in list:
+        if int(i) % 2 != 0:
+            return False
+    return True
+
+
+def get_longest_all_even(list):
+    subsec = []
+    for i in range(len(list)):
+        for j in range(i, len(list)):
+            if numerepare(list[i:j + 1]) == True and len(list[i:j + 1]) > len(subsec):
+                subsec = list[i:j + 1]
+
+    return subsec
+
 
 def test_get_longest_sum_is_prime():
-    assert (get_longest_sum_is_prime(["4","2","1","3"])==["4","2","1"])
-    assert (get_longest_sum_is_prime(["1","2"])==["1","2"])
-    assert (get_longest_sum_is_prime(["1","2","3","4","5","6"])==["1","2"])
+    assert (get_longest_sum_is_prime(["4", "2", "1", "3"]) == ["4", "2", "1"])
+    assert (get_longest_sum_is_prime(["1", "2"]) == ["1", "2"])
+    assert (get_longest_sum_is_prime(["1", "2", "3", "4", "5", "6"]) == ["1", "2"])
+
 
 def test_get_longest_digit_count_desc():
-    assert (get_longest_digit_count_desc(["1234","123","1","1234"])==["1234","123","1"])
-    assert (get_longest_digit_count_desc(["1234","12"])==["1234","12"])
-    assert (get_longest_digit_count_desc(["1234","1","123","12","1","123412"])==["123","12","1"])
+    assert (get_longest_digit_count_desc(["1234", "123", "1", "1234"]) == ["1234", "123", "1"])
+    assert (get_longest_digit_count_desc(["1234", "12"]) == ["1234", "12"])
+    assert (get_longest_digit_count_desc(["1234", "1", "123", "12", "1", "123412"]) == ["123", "12", "1"])
+
+
+def test_get_longest_all_even():
+    assert (get_longest_all_even(["1","3","5"])==[])
+    assert (get_longest_all_even(["1", "2", "3", "4", "8", "6", "5", "6"]) == ["4", "8", "6"])
+    assert (get_longest_all_even(["1","2","3","4","5"])==["2"])
 
 
 def citire():
@@ -101,40 +127,38 @@ def citire():
     return list
 
 
-
 def main():
     while True:
 
         print(test_get_longest_sum_is_prime())
         print(test_get_longest_digit_count_desc())
-
+        print(test_get_longest_all_even())
 
         print("1.Cititi numerele: ")
         print("x.Iesire")
 
-
         optiune = input("Dati optiune: ")
-        if optiune=="1":
+        if optiune == "1":
             list = []
-            list=citire()
+            list = citire()
 
             print("1.Determinati cea mai lunga secventa in care suma numerelor este numar prim.")
             print("2.Determinati cea mai lunga secventa in care numarul de cifre este in ordine descrescatoare.")
+            print("3.Determinati cea mai lunga secventa in care numerele sunt pare")
 
-            optiune2=input("Dati optiune: ")
-            if optiune2=="1":
+            optiune2 = input("Dati optiune: ")
+            if optiune2 == "1":
                 print(get_longest_sum_is_prime(list))
-            elif optiune2=="2":
+            elif optiune2 == "2":
                 print(get_longest_digit_count_desc(list))
+            elif optiune2 == "3":
+                print(get_longest_all_even(list))
 
-        elif optiune=="x":
+        elif optiune == "x":
             break
         else:
             print("Optiune gresita! Rincearca")
 
 
-
 if __name__ == '__main__':
     main()
-
-
